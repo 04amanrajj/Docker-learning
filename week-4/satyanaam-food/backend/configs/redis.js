f4e5f95f77995260c7606f92f07a5b2564a75e01
04amanrajj <04.aman.raj@gmail.com>
2026-03-18 18:00:00 +0530

    refactor(showcase): Purge educational source folders to isolate static portfolio files at root

--- week-4/satyanaam-food/backend/configs/redis.js
+++
@@ -1,6 +0,0 @@
-const redis = require("redis");
-require("dotenv").config();
-
-const redisUri = process.env.REDIS_URI || "redis://127.0.0.1:6379";
-exports.client = redis.createClient({ url: redisUri });
-
--- week-4/satyanaam-food/backend/models/blacklistTokens.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const blacklistToken = mongoose.Schema({
-  token: String,
-},{ versionKey: false });
-
-const BlackListToken = mongoose.model("blackListedToken", blacklistToken);
-
-module.exports = { BlackListToken };
